3d8a39198185986503c364dc63dd1ff2ad74ad26
Moises Herrera <moisesherrera@Moisess-MacBook-Air.local>
2024-02-15 16:29:23 -0600

    proxyrevision

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
-const proxy = require('http-proxy-middleware')
+const { proxy } = require('http-proxy-middleware');
 
 module.exports = function(app){
-    app.use(proxy('/generate', { target: 'https://glrsc-system-1-27a1742ceb52.herokuapp.com'}));
-    app.use(proxy('/login_token', { target: 'https://glrsc-system-1-27a1742ceb52.herokuapp.com'}));
-    app.use(proxy('/delete', { target: 'https://glrsc-system-1-27a1742ceb52.herokuapp.com'}));
-    app.use(proxy('/password', { target: 'https://glrsc-system-1-27a1742ceb52.herokuapp.com'}));
-    app.use(proxy('/logout', { target: 'https://glrsc-system-1-27a1742ceb52.herokuapp.com'}));
-    app.use(proxy('/account', { target: 'https://glrsc-system-1-27a1742ceb52.herokuapp.com'}));
-    app.use(proxy('/store_location', { target: 'https://glrsc-system-1-27a1742ceb52.herokuapp.com'}));
+    app.use(proxy('/generate', { target: 'https://glrsc-system-1-27a1742ceb52.herokuapp.com', changeOrigin: true}));
+    app.use(proxy('/login_token', { target: 'https://glrsc-system-1-27a1742ceb52.herokuapp.com', changeOrigin: true}));
+    app.use(proxy('/delete', { target: 'https://glrsc-system-1-27a1742ceb52.herokuapp.com', changeOrigin: true}));
+    app.use(proxy('/password', { target: 'https://glrsc-system-1-27a1742ceb52.herokuapp.com', changeOrigin: true}));
+    app.use(proxy('/logout', { target: 'https://glrsc-system-1-27a1742ceb52.herokuapp.com', changeOrigin: true}));
+    app.use(proxy('/account', { target: 'https://glrsc-system-1-27a1742ceb52.herokuapp.com', changeOrigin: true}));
+    app.use(proxy('/store_location', { target: 'https://glrsc-system-1-27a1742ceb52.herokuapp.com', changeOrigin: true}));
 }
